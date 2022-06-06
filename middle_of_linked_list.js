5c6c93673d169649c57dd8e72cfe4b2bb3019745
humbejeff2116 <humbejeff2116@gmail.com>
2022-06-06 08:51:02 +0100

    add a more peformant solution

--- middle_of_linked_list.js
+++ middle_of_linked_list.js
@@ -43,3 +43,53 @@ var middleNode = function(head) {
     }
     return head;
 };
+
+
+//more prfomant and low memory consumption solution
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+var middleNode = function(head) {
+    let slow = head;
+    let fast = head;
+    
+    // ODD number of nodes:
+    
+    // first step:
+    //     s  f
+    // [1, 2, 3, 4, 5]
+    
+    // second step:
+    //        s     f
+    // [1, 2, 3, 4, 5]
+    
+    // EVEN number of nodes:
+    
+    // first step:
+    //     s  f
+    // [1, 2, 3, 4]
+    
+    // second step:
+    //        s     f
+    // [1, 2, 3, 4]
+    
+    // fast node will always move 2 nodes each iteration
+    // slow node will move 1 node each iteration
+    
+    
+    // check to make sure fast node is not the last node
+    while (fast !==null && fast.next !== null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    return slow;
+};
